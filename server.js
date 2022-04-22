1ed7c1bcc3b159984a3b41268aec1a41e62e751e
jadashi97 <njadauniversal23@yahoo.com>
2022-04-22 11:43:52 -0500

    (feat): more work on routes and getting familiar with nodemon

--- server.js
+++ server.js
@@ -7,12 +7,29 @@ const app = express();
 
 //this is used when you want to make a request or if the user wants a response  from the server
 
+//this is a request to the home route
 app.get("/", function(request, response){
     response.send("<h1>aloha, Shalom Welcome to my World!!!!</h1>");
+});
+
+//this is a request to the contact route
+app.get("/contact", function(req, res){
+    res.send("hit me up  on twitter@jadakoseD")
+});
+
+//this is a request to the my bio route
+
+app.get("/about", function(req, res) {
+    res.send("I am a black 5'11 male. I like sports and just minding my business.")
+});
+
+//this is a request to the my hobbies route
+app.get("/hobbies", function(req,res){
+    res.send("code, podcasts, books, soccer, rugby, beer, pork");
 });
 
 // this is used when requesting infor from a server
 
 app.listen(3000, function() {
     console.log(`Example app listening on port ${3000}`);
-});
+}); 
